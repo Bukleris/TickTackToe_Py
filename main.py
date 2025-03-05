9af63dbe111d92b4bc4a3f31b4f74c45fa5e3bd7
Bukleris <85689682+Bukleris@users.noreply.github.com>
2025-03-05 16:29:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-
+import random
+#draw the game board
+def printGameBoard(board):
+    print("\n")
+    print(f"  {board[7]} | {board[8]} | {board[9]}")
+    print(" ---+---+---")
+    print(f"  {board[4]} | {board[5]} | {board[6]}")
+    print(" ---+---+---")
+    print(f"  {board[1]} | {board[2]} | {board[3]}")
+    print("\n")
